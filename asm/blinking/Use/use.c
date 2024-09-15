b8ded2184a2ebb3c08c6e8d7caced3d848878595
nghiank97 <nghia.nk97@gmail.com>
2024-09-15 20:17:01 +0700

    ams

---
+++ asm/blinking/Use/use.c
@@ -0,0 +1,23 @@
+#include "binary.h"
+#include "use.h"
+
+#define LED_OFF() 			{GPIOA->ODR |= (1<<1);}
+#define LED_ON() 				{GPIOA->ODR &=~ (1<<1);}
+#define LED_TOGGLE() 		{GPIOA->ODR ^= (1<<1);}
+
+extern void setup(void){
+	// enable clock GPIO A
+	RCC->AHB4ENR |= RCC_AHB4ENR_GPIOAEN;
+	// general purpose output mode
+	GPIOA->MODER |= (B01<<2);
+	// output push-pull
+	GPIOA->OTYPER &=~ (B1<<1);
+	// Medium speed
+	GPIOA->OSPEEDR |= (B01<<2);
+}
+
+extern void loop(void){
+	LED_TOGGLE();
+	HAL_Delay(200);
+}
+
